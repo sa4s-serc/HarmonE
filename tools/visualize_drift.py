c19ca0ed4216729fec498b424f2f0ec4302c177a
StarryBadger <shaunakbiswas@gmail.com>
2025-03-10 10:58:29 +0530

    feat: system prep for linear, test file has drift

--- tools/visualize_drift.py
+++ tools/visualize_drift.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import entropy, wasserstein_distance
 
 # Load dataset
-df = pd.read_csv("data/pems/flow_data_drifted.csv")
+df = pd.read_csv("data/pems/flow_data_test.csv")
 df.columns = df.columns.str.strip()  # Strip any whitespace from column names
 
 # Parameters
