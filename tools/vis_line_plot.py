388f66884284e2b0d70f21d9cd19b93f89a753ef
StarryBadger <shaunakbiswas@gmail.com>
2025-03-12 22:31:43 +0530

    feat: saved progress for 4

--- tools/vis_line_plot.py
+++ tools/vis_line_plot.py
@@ -7,7 +7,7 @@ models = {
     "Linear Regression": "output/updated/modified_predictions_linear.csv",
     "SVM": "output/updated/modified_predictions_svm.csv",
     "LSTM": "output/updated/modified_predictions_lstm.csv",
-    # "Switching": "output/runs/predictions_4_1.csv",
+    "Switching": "output/runs/predictions_4_3.csv",
     "Switching*": "output/runs/predictions_4_2.csv",
     "Retraining": "output/runs/predictions_8_1.csv",
     # "Retraining*": "output/runs/predictions_8_2.csv",
